d5f16302ac5a3173b277afb6cca0bfe420ec5116
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-06-03 12:09:33 +0200

    Styled successpage
    styled and added return to home button

--- src/pages/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export function CheckoutSuccess() {
+
   return (
-    <h1>Success</h1>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+        <h1 className="text-4xl font-bold text-green-600 mb-4">Success</h1>
+        <p className="text-lg text-gray-700 mb-6">Your order has been placed successfully!</p>
+        <Link to={`/`}>
+          <button className="px-6 py-3 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"> Go to Home Page </button>
+        </Link>
+        
+      </div>
+    </div>
+  );
 }
