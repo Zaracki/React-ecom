d2ab1cffdec566f43d760edacd90bb86124a0c01
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-06-03 08:05:19 +0200

    Moved card into right folder

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import DiscountDisplay from './components/DiscountDisplay';
+import DiscountDisplay from '../DiscountDisplay';
 
 const Card = ({ data }) => {
   const { id, image, title, price, discountedPrice } = data;
