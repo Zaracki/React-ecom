8257bb5fdf5391e33d3dcc0c81d731aeddcf0e68
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-06-09 19:39:34 +0200

    name change for netlify

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Checkout } from './pages/Checkout';
 import { Contact } from './pages/Contact';
 import { ProductPage } from './pages/ProductPage';
 import './index.css'; 
-import { Layout } from './components/navigation/Layout';
+import { Layout } from './components/Navigation/Layout';
 import { CheckoutSuccess } from './pages/CheckoutSuccess';
 
 function App() {
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../navigation/Navbar";
+import Navbar from "../Navigation/Navbar";
 
 export default function Header() {
   return (
