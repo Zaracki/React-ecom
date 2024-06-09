80f0cc8ebd1a76cb8e2f96f85101fc8aeac72dde
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-06-09 19:43:32 +0200

    layout fix

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,5 @@
-import Header from "../Layout/Header";
-import Footer from "../Layout/Footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
