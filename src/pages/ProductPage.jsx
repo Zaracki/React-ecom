3afd719889470f32dba98c6e185e2068d9d6a53d
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-09-09 07:51:50 +0200

    Update ProductPage.jsx

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -38,7 +38,7 @@ export function ProductPage() {
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+    <main className="min-h-screen flex items-center justify-center">
       <div className="max-w-4xl bg-white rounded-xl overflow-hidden shadow-lg flex">
         <ProductImage imageUrl={image.url} altText={title} />
         <div className="p-8">
